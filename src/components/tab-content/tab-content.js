f1133f93d7611c19c1f6ecb92cecaa96b0335f65
andra <andra.olah@gmail.com>
2016-12-14 16:15:44 +0200

    added tab content

---
+++ src/components/tab-content/tab-content.js
@@ -0,0 +1,36 @@
+/**
+ * Created by Andra on 01-Nov-16.
+ */
+import React, { Component, PropTypes } from 'react';
+import {} from './tab-content.less';
+import Subtab from '../subtab/subtab';
+
+export default class TabContent extends Component {
+
+    constructor(props) {
+        super(props);
+        this.onClickSubtabButtonHandler = this.onClickSubtabButtonHandler.bind(this);
+    }
+
+    onClickSubtabButtonHandler(id) {
+        
+    }
+
+    render() {
+        const tabs = this.props.tabs.map((subtabData) =>
+             <Subtab {...subtabData} onClickSubtabButtonHandler={this.onClickSubtabButtonHandler} />
+        );
+
+        return (
+            <div className="tab-content">
+                <div className="title">{this.props.title}</div>
+                {tabs}
+            </div>
+        );
+    }
+}
+
+TabContent.propTypes = {
+    title: PropTypes.string.isRequired,
+    tabs: PropTypes.arrayOf(PropTypes.object).isRequired,
+};
---
+++ tests/tabContent.spec.js
@@ -0,0 +1,46 @@
+/**
+ * Created by Andra on 16-Nov-16.
+ */
+import React from 'react';
+import { shallow, mount } from 'enzyme';
+import { expect } from 'chai';
+import './helpers/chai-enzyme';
+import TabContent from '../src/components/tab-content/tab-content';
+import Subtab from '../src/components/subtab/subtab';
+
+describe('<TabContent />', () => {
+    let myTabContent;
+
+    const subTabProps = [
+        {
+            id: 1,
+            selected: true,
+            label: 'Shopping',
+        },
+        {
+            id: 2,
+            selected: false,
+            label: 'Social',
+        },
+        {
+            id: 3,
+            selected: false,
+        },
+    ];
+
+    it('has title prop', () => {
+        myTabContent = shallow(
+            <TabContent title="Title" tabs={subTabProps} />
+        );
+        expect(myTabContent.find('.title')).to.not.be.blank();
+    });
+
+    it('has subtabs', () => {
+        myTabContent = mount(
+            <TabContent title="Title" tabs={subTabProps} />
+        );
+        subTabProps.forEach((prop) => {
+            expect(myTabContent).to.contain(<Subtab key={prop.id} {...prop} onClickSubtabButtonHandler={myTabContent.instance().onClickSubtabButtonHandler} />);
+        });
+    });
+});
